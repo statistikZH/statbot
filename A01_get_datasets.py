71403242637fc841c7f78f44bb14bc6bf5604dd0
ruizcrp <ruiz.crp@gmail.com>
2021-03-19 18:12:59 +0100

    description

--- A01_get_datasets.py
+++ A01_get_datasets.py
@@ -1,3 +1,15 @@
+### Description of script
+#All publicly available data (Open Government Data – OGD) of the Statistical Office of the Canton of Zurich
+#are extracted from the central JSON-file. By downloading and opening up every file, additional data can be
+#extracted such as variable names that are not described in the metadata. Extracted is thus a combination
+#between metadata and some information in the dataset.
+
+# thoughts for improvement
+# - much more metadata could be extracted
+# - This could later be improved to save the data in a mysql-db.
+
+
+
 import pandas as pd
 import json
 from urllib.request import urlopen
@@ -6,14 +18,16 @@ import wget
 import os
 import io
 
-#import json
+#import the central json file
 url="https://www.web.statistik.zh.ch/data/zhweb.json"
 json_url = urlopen(url)
 data = json.load(json_url)
 list_data=[]
 for i,entry in enumerate(data['dataset']):
+    #get for every dataset the urlname that is written in the respective node
     urlname=entry['distribution'][0]['downloadUrl']
     if urlname is not None:
+        #select only the csv files, as there are also other kind of files
         if urlname[-4:]==".csv":
             filename='data/'+str(i)+'.csv'
             if os.path.exists(filename):
@@ -24,7 +38,9 @@ for i,entry in enumerate(data['dataset']):
                 print(i,"Download failed")
             mydelimiter=None
 
+            #there was an issue with the encoding of the files, this is why latin-1 is used
             with io.open(filename, 'r', encoding="latin-1") as csvfile:
+                #also we need an own csv-sniffer to well observe whether the file is ,-delimited or ;-delimited - both are currently used
                 dialect = csv.Sniffer().sniff(csvfile.readline(), [',',';'])
                 csvfile.seek(0)
                 mydelimiter=dialect.delimiter
@@ -38,7 +54,7 @@ for i,entry in enumerate(data['dataset']):
 
     print(i)
 
-
+#and the file with all the relevant metadata is saved here
 df = pd.DataFrame(list_data, columns=['index', 'dataset_title', 'description','var','nunique','dtype'])
 df.to_csv('data/datasets_overview.csv',index=False)
 print("FINISHED")
