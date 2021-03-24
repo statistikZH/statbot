09c0e60cb48890c2cbcaca1e66e964297bbcacf8
ruizcrp <ruiz.crp@gmail.com>
2021-03-24 21:55:43 +0100

    difficult gold questions

---
+++ K01_wahl.py
@@ -0,0 +1,9 @@
+#korrekturscript
+#jedes Korrektur modul wird eingelesen
+
+#kantonsratswahlen... etc.
+list_data=["78@statistisches-amt-kanton-zuerich","79@statistisches-amt-kanton-zuerich","80@statistisches-amt-kanton-zuerich","7@statistisches-amt-kanton-zuerich","8@statistisches-amt-kanton-zuerich","9@statistisches-amt-kanton-zuerich","10@statistisches-amt-kanton-zuerich",
+"288@statistisches-amt-kanton-zuerich","304@statistisches-amt-kanton-zuerich","305@statistisches-amt-kanton-zuerich","380@statistisches-amt-kanton-zuerich"]
+
+#loop over files... maybe index necessary
+#merge to one file.
