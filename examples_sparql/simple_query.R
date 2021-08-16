c9db1826a60226d987370a6e025c46c61005a668
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-08-16 18:53:07 +0200

    added example for SPARQL query

---
+++ examples_sparql/simple_query.R
@@ -0,0 +1,33 @@
+library(SPARQL) # SPARQL querying package
+library(ggplot2)
+# Step 1 - Set up preliminaries and define query
+# Define the data.gov endpoint
+#endpoint <- "http://datashop.westeurope.cloudapp.azure.com/statistics/sparql"
+endpoint <- "http://datashop.westeurope.cloudapp.azure.com/statistics/query"
+# create query statement
+query <-
+  "
+PREFIX schema: <http://schema.org/>
+PREFIX cube: <https://cube.link/>
+PREFIX property: <http://datashop.westeurope.cloudapp.azure.com/statistics/property/>
+
+#SELECT ?time ?place ?count
+SELECT ?obs ?count ?place ?time ?id ?count ?zeit
+
+WHERE {
+  <http://datashop.westeurope.cloudapp.azure.com/statistics/DS0048> a cube:Cube;
+             cube:observationSet/cube:observation ?obs.
+    ?obs schema:identifier ?id .
+  OPTIONAL{         ?obs property:RAUM ?place .}
+  ?obs property:TIME  ?time .
+  ?obs property:ZEIT  ?zeit .
+  OPTIONAL{ ?obs <http://datashop.westeurope.cloudapp.azure.com/statistics/measure/M0048> ?count .}
+}
+
+"
+
+
+
+# Step 2 - Use SPARQL package to submit query and save results to a data frame
+qd <- SPARQL(endpoint,query)
+df <- qd$results
