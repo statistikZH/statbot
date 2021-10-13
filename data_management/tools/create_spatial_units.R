d6e8cd21a1b1c37165f16f8588bc64ddf8fd73bd
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-10-13 11:41:08 +0200

    improving spatial units csv

--- data_management/tools/create_spatial_units.R
+++ data_management/tools/create_spatial_units.R
@@ -5,4 +5,8 @@ df$TYPE_NAME<-ifelse(df$TYPE_ID==8,"Kanton",df$TYPE_NAME)
 df$TYPE_NAME<-ifelse(df$TYPE_ID==4,"Region",df$TYPE_NAME)
 df$TYPE_NAME<-ifelse(df$TYPE_ID==3,"Bezirk",df$TYPE_NAME)
 df$TYPE_NAME<-ifelse(df$TYPE_ID==9,"Gemeinde alt",df$TYPE_NAME)
-names(df)[]
+colnames(df)[colnames(df)=="NAME"]<-"NAME_DE"
+df$NAME_FR<-df$NAME_DE
+df$NAME_IT<-df$NAME_DE
+df$NAME_EN<-df$NAME_DE
+write.csv(df,"data/spatialunits.csv")
