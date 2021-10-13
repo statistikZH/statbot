206fcfc2ab1b24c3f267565963e9b4df616eee73
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-10-13 10:32:58 +0200

    template modifications

--- data_management/src/template.R
+++ data_management/src/template.R
@@ -1,4 +1,10 @@
-statbot_src_XXX <- function(){
+# Schema for script name and function name
+# X_X_YYY_ZZZZ
+# X_X=SDMX-Classification
+# YYY=STATBOT_CODE
+# ZZZZ=CH FOR OVERALL COUNTRY CODE, OR YOUR 2-LETTER CANTONAL CODE OR FOUR DIGITS BFS-COMMUNAL-CODE
+
+statbot_src_X_X_YYY_ZZZZ <- function(){
   print("hi1")
   return("OK")
 }
--- data_management/src/template.py
+++ data_management/src/template.py
@@ -1,3 +1,9 @@
+# Schema for script name
+# X_X_YYY_ZZZZ
+# X_X=SDMX-Classification
+# YYY=STATBOT_CODE
+# ZZZZ=CH FOR OVERALL COUNTRY CODE, OR YOUR 2-LETTER CANTONAL CODE OR FOUR DIGITS BFS-COMMUNAL-CODE
+
 import sys
 print("hello world")
 sys.exit(0)
