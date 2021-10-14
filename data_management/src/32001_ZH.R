164535d12422e16ff67f1f4aec59bf6af5c07f72
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-10-14 11:35:19 +0200

    32001_ZH coming

---
+++ data_management/src/32001_ZH.R
@@ -0,0 +1,14 @@
+source("helper.R")
+library("janitor")
+library("tidyverse")
+
+#consists of communal (+swiss total) and of cantonal areas
+
+statbot_src_32001_ZH <- function(flag_force_update=FALSE){
+
+
+
+
+  return("UPDATE OK")
+
+}
