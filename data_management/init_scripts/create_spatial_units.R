dbe069d1b8a7e0779f8dc028c3b268c2600f64fe
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-10-15 10:05:41 +0200

    cantons

--- data_management/init_scripts/create_spatial_units.R
+++ data_management/init_scripts/create_spatial_units.R
@@ -4,7 +4,8 @@ ch<-read.csv("data/ch_gemeinden.csv",col.names = c("SPATIALUNIT_ID","NAME"))
 df<-df[,1:4]
 
 # 15.10.2021: Temporary workaround: Taking Gemeinde values from an outdated BFS file
-df<-df[df$TYPE_ID!=1,]
+# also take the cantons out and make it later clean
+df<-df[df$TYPE_ID!=1 & df$TYPE_ID!=8,]
 ch$BFS_NR<-ch$SPATIALUNIT_ID
 ch$TYPE_ID<-1
 df<-rbind(df,ch)
@@ -31,7 +32,7 @@ df$name_en<-df$name_de
 
 # new spatial units
 df$spatialunit_id<-ifelse(df$type_id==1|df$type_id==9,df$bfs_nr,df$spatialunit_id)
-df$spatialunit_id<-ifelse(df$type_id==8&df$name_de=="Zürich - ganzer Kanton",010000,df$spatialunit_id)
+df$spatialunit_id<-ifelse(df$type_id==8&df$name_de=="Kanton Zürich - ganzer Kanton",010000,df$spatialunit_id)
 df$spatialunit_id<-ifelse(df$type_id==8&df$name_de=="Kanton Basel-Stadt",120000,df$spatialunit_id)
 df$spatialunit_id<-ifelse(df$type_id==3|df$type_id==4,df$spatialunit_id+010000,df$spatialunit_id)
 df$spatialunit_id<-ifelse(df$type_id==10,df$spatialunit_id+120000,df$spatialunit_id)
@@ -39,7 +40,13 @@ df$spatialunit_id<-ifelse(df$type_id==10,df$spatialunit_id+120000,df$spatialunit
 df<-rbind(df,data.frame(spatialunit_id=0,type_id=0,name_de="Schweiz",bfs_nr=NA,type_name="Schweiz",
                         name_fr="Suisse",name_it="Svizzera",name_en="Switzerland"))
 
-
+df<-rbind(df,data.frame(spatialunit_id=seq(010000,260000,10000),type_id=8,
+      name_de=c("Kanton Zürich","Kanton Bern","Kanton Luzern","Kanton Uri","Kanton Schwyz","Kanton Obwalden","Kanton Nidwalden","Kanton Glarus","Kanton Zug","Kanton Freiburg","Kanton Solothurn","Kanton Basel-Stadt","Kanton Basel-Landschaft","Kanton Schaffhausen","Kanton Appenzell Ausserrhoden","Kanton Appenzell Innerrhoden","Kanton St. Gallen","Kanton Graubünden","Kanton Aargau","Kanton Thurgau","Kanton Tessin","Kanton Waadt","Kanton Wallis","Kanton Neuenburg","Kanton Genf","Kanton Jura"),
+      bfs_nr=NA,type_name="Kanton",
+      name_fr=c("Canton de Zurich","Canton de Berne","Canton de Lucerne","Canton d'Uri","Canton de Schwyz","Canton d'Obwald","Canton de Nidwald","Canton de Glaris","Canton de Zug","Canton de Fribourg","Canton de Soleure","Canton de Bâle-Ville","Canton de Bâle-Campagne","Canton de Schaffhouse","Canton d'Appenzell Rhodes-Extérieures","Canton d'Appenzell Rhodes-Intérieures","Canton de St. Gallen","Canton des Grisons","Canton d'Argovie","Canton de Thurgovie","Canton du Tessin","Canton de Vaud","Canton du Valais","Canton de Neuchâtel","Canton de Genève","Canton du Jura"),
+      name_it=c("Cantone di Zurigo","Cantone di Berna","Cantone di Lucerna","Cantone di Uri","Cantone di Svitto","Cantone di Obvaldo","Cantone di Nidvaldo","Cantone di Glarona","Cantone di Zugo","Cantone di Friburgo","Cantone di Soletta","Cantone di Basilea Città","Cantone di Basilea Campagna","Cantone di Sciaffusa","Cantone di Appenzello Esterno","Cantone di Appenzello Interno","Cantone di St. Gallen","Canton Grigioni","Canton Argovia","Canton Turgovia","Canton Ticino","Canton Vaud","Canton Vallese","Canton Neuchâtel","Canton Ginevra","Canton Giura"),
+      name_en=c("Canton of Zurich","Canton of Bern","Canton of Lucerne","Canton of Uri","Canton of Schwyz","Canton of Obwalden","Canton of Nidwalden","Canton of Glarus","Canton of Zug","Canton of Fribourg","Canton of Solothurn","Canton of Basel-Stadt","Canton of Basel-Landschaft","Canton of Schaffhausen","Canton of Appenzell Ausserrhoden","Canton of Appenzell Innerrhoden","Canton of St. Gallen","Canton Graubünden","Canton Aargau","Canton Thurgau","Canton Ticino","Canton Vaud","Canton Valais","Canton Neuchâtel","Canton Geneva","Canton Jura")
+      ))
 
 
 
