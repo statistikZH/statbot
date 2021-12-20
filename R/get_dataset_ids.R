512fb903d46cb63a40a44ca4652898044377a0fa
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-12-20 17:28:49 +0100

    changing this helper-function to the new logic

--- R/get_dataset_ids.R
+++ R/get_dataset_ids.R
@@ -1,5 +1,7 @@
 get_dataset_ids <- function(){
-  function_files <- list.files("R/",pattern = "^[0-9][0-9][0-9][0-9].*$")
+  # 20.12.2021: Change of ID-logic according to V3 of DB-schema
+  #function_files <- list.files("R/",pattern = "^[0-9][0-9][0-9][0-9].*$")
+  function_files <- list.files("R/",pattern = "^[0-9]_[0-9][0-9]_[0-9][0-9][0-9].*$")
 
   dataset_ids <- gsub("\\.R", "", function_files)
 
