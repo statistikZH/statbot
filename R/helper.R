726a96c607db80e428f0b6b51a019a939186e1c7
Christian Ruiz <christian.ruiz@statistik.ji.zh.ch>
2021-12-21 14:59:55 +0100

    sourcing out

--- R/helper.R
+++ R/helper.R
@@ -1,11 +1,5 @@
 
 
-#GLOBAL_TOTAL_LIST<-c("spatialunit_ontology","spatialunit_hist_id","spatialunit_name",
-#                  "time_value","period_value","value")
-
-
-
-
 update_last_updated<-function(id){
   df<-read.csv("data/classes.csv")
 
@@ -15,10 +9,17 @@ update_last_updated<-function(id){
   write.csv(df,"data/classes.csv",row.names = F)
 }
 
-# TODO VERY WRONG
-convert_and_write_per_unit<-function(df,new_id,file_name,how_many=1000){
-  df$value<-df$value/how_many
-  df$indicator_id<-new_id
+
+convert_and_write_per_unit<-function(df,file_name,how_many=1000,type="pop"){
+  pop<-fread("data/values/1_01_001.csv",select = c("spatialunit_ontology","spatialunit_hist_id","value","time_value"))
+  pop<-pop %>% rename(pop=value)
+
+  # needs to have exact time_value like 31.12.2020
+  df<-merge(df,pop,by=c("spatialunit_ontology","spatialunit_hist_id","time_value"),all.x=T,sort=F)
+
+  df$value<-df$value/df$pop*how_many
+  df$pop<-NULL
+
   write.csv(df,paste0("data/values/",file_name),row.names = F)
 
 }
